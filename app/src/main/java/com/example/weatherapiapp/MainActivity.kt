53204438354bafef7cb0a70649ca7cac8e5c4db7
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2022-04-26 23:10:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/weatherapiapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+  //  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
+}
